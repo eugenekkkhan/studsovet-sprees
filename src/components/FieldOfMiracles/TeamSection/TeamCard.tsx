abfa5c59aa61d7b0f8347803049eabcc79120bbf
eugenekkkhan <eugenekhananov@yandex.ru>
2026-04-09 06:35:57 +0300

    refactor: remove unused addOpacity function from TeamCard component

--- src/components/FieldOfMiracles/TeamSection/TeamCard.tsx
+++ src/components/FieldOfMiracles/TeamSection/TeamCard.tsx
@@ -26,13 +26,6 @@ const makeMoreContrast = (color: string) => {
   }
 };
 
-const addOpacity = (color: string, opacity: number) => {
-  const r = parseInt(color.slice(1, 3), 16);
-  const g = parseInt(color.slice(3, 5), 16);
-  const b = parseInt(color.slice(5, 7), 16);
-  return `rgba(${r}, ${g}, ${b}, ${opacity})`;
-};
-
 const TeamCard = ({
   team,
   setPoints,
