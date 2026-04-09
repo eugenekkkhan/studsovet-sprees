85c5599750c58cafac0299f74cd13f5203a7f725
eugenekkkhan <eugenekhananov@yandex.ru>
2026-04-09 05:49:58 +0300

    refactor: simplify Header component by extracting link rendering logic

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,42 @@
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 import "./Header.css";
+
+interface LinkItem {
+  text: string;
+  path: string;
+}
+
+const renderLinks = ({
+  currentPath,
+  links,
+}: {
+  currentPath: string;
+  links: LinkItem[];
+}) => {
+  return links.map((link) => (
+    <Link
+      key={link.path}
+      to={link.path}
+      className={`header-link ${currentPath === link.path ? "active" : ""}`}
+    >
+      {link.text}
+    </Link>
+  ));
+};
+
 const Header = () => {
+  const location = useLocation();
+  const currentPath = location.pathname;
+
+  const links = [
+    { text: "Колесо удачи", path: "/roulette" },
+    { text: "Поле чудес", path: "/field-of-miracles" },
+  ];
+
   return (
     <div className="header">
       <div className="header-container">
-        <Link to="/roulette" className="header-title">
-          Колесо удачи
-        </Link>
-        <Link to="/field-of-miracles" className="header-link">
-          Поле чудес
-        </Link>
+        {renderLinks({ currentPath, links })}
       </div>
     </div>
   );
